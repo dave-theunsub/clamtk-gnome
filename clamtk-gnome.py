3c0711e3c87ef804c6445e3c7e972fcb3b923bed
dave-theunsub <davem@davem.codes>
2020-02-09 09:44:06 -0600

    update to 0.04

--- clamtk-gnome.py
+++ clamtk-gnome.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# ClamTk, copyright (C) 2004-2019 Dave M, Tord Dellsén
+# ClamTk, copyright (C) 2004-2020 Dave M, Tord Dellsén
 #
 # This file is part of ClamTk
 # (https://gitlab.com/dave_m/clamtk/wikis/Home)
@@ -13,20 +13,25 @@
 #
 # b) the "Artistic License".
 
-import os
-try:
-    # Python 3
-    from shlex import quote
-except ImportError:
-    # Python 2
-    from pipes import quote
+import subprocess
+from shlex import quote
+
 import locale
-locale.setlocale(locale.LC_ALL, '')
+
+locale.setlocale(locale.LC_ALL, "")
 
 import gettext
-_ = lambda x: gettext.ldgettext("clamtk-gnome", x)
 
-from gi.repository import Nautilus, GObject
+gettext.bindtextdomain("clamtk", "/usr/share/locale")
+gettext.textdomain("clamtk")
+_ = gettext.gettext
+
+import gi
+
+gi.require_version("Nautilus", "3.0")
+
+from gi.repository import Nautilus
+from gi.repository import GObject
 
 
 class OpenTerminalExtension(GObject.GObject, Nautilus.MenuProvider):
@@ -44,13 +49,13 @@ class OpenTerminalExtension(GObject.GObject, Nautilus.MenuProvider):
         # which has the get_path function which returns the absolute path as a string
 
         filename = quote(filename)
-
-        os.system('clamtk %s &' % filename)
+        cmd = ["clamtk", filename]
+        subprocess.Popen(cmd)
 
     def menu_activate_cb(self, menu, file):
         self._open_scanner(file)
 
-    def menu_background_activate_cb(self, menu, file): 
+    def menu_background_activate_cb(self, menu, file):
         self._open_scanner(file)
 
     def get_file_items(self, window, files):
@@ -59,24 +64,26 @@ class OpenTerminalExtension(GObject.GObject, Nautilus.MenuProvider):
         file = files[0]
 
         item = Nautilus.MenuItem(
-            name='NautilusPython::openscanner',
-            label=_('Scan for threats...') ,
-            tip=_('Scan %s for threats...') % file.get_name(),
-            icon='clamtk')
+            name="NautilusPython::openscanner",
+            label=_("Scan for threats..."),
+            tip=_("Scan for threats..."),
+            icon="clamtk",
+        )
         # - the tooltips are not shown any longer in Nautilus
-        # (the code is kept here in case this changes again for Nautilus
-        item.connect('activate', self.menu_activate_cb, file)
+        # (the code is kept here in case this changes again for Nautilus)
+        item.connect("activate", self.menu_activate_cb, file)
 
         return [item]
 
     def get_background_items(self, window, file):
         item = Nautilus.MenuItem(
-            name='NautilusPython::openscanner_directory',
-            label=_('Scan directory for threats...'),
-            tip=_('Scan this directory for threats...'),
-            icon='clamtk')
+            name="NautilusPython::openscanner_directory",
+            label=_("Scan directory for threats..."),
+            tip=_("Scan this directory for threats..."),
+            icon="clamtk",
+        )
         # - the tooltips are not shown any longer in Nautilus
         # (the code is kept here in case this changes again for Nautilus
-        item.connect('activate', self.menu_background_activate_cb, file)
+        item.connect("activate", self.menu_background_activate_cb, file)
 
         return [item]
